b1c336acb2a7e441f00234bead8d08957b43fd3f
jafar260698 <jafar_temirov@mail.ru>
2022-08-12 16:40:42 +0500

    - test

--- test/create_qr_test.dart
+++ test/create_qr_test.dart
@@ -4,9 +4,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Create Hash', () {
-    var text =
-        'AB7BFF228997A3A9356E90A034B82F1AAF0D4FC0391D6DE1B2D13C7ED20CF7A7'; // Challenge
-    var docHash = GostHash.hashGost(text);// challange hex
+    var raw =
+        'AB7BFF228997A3A9356E90A034B82F1AAF0D4FC0391D6DE1B2D13C7ED20CF7A7'.codeUnits; // Challenge
+    var docHash = GostHash.hashGost2Hex(raw);
     var crc32 = Crc32.calcHex(docHash);
     docHash += crc32;
     print("crc32: $crc32");
